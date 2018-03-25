febf21e955141e34103679ece03225f38a94c92a
Robin <robin.neu@devinci.fr>
2018-03-25 12:43:50 +0100

    feat(populate): search index

--- server/index.js
+++ server/index.js
@@ -4,7 +4,21 @@ const app = express();
 const populate = require('./mods/populate')
 const suv = require('./mods/suv')
 
+app.use(bodyParser.json())
 
+// const router = express.Router()
+
+app.get('/populate', (req, res) => {
+  populate.addInElastic((err, results) => {
+    res.json(results);
+  })
+})
+
+app.get('/suv', (req, res) => {
+  suv.sortSuv((err, results) => {
+    res.json(results)
+  })
+})
 
 const port = 9292
 app.listen(port, () => {
